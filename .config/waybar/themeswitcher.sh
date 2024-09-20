a6231606ba6632d279441d73a7d3b8b6259fc1e1
Alexey Scorpionov <scorpp13@gmail.com>
2024-09-20 17:29:39 +0000

    Replace themeswitcher.sh

--- .config/waybar/themeswitcher.sh
+++ .config/waybar/themeswitcher.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #  _____ _                  ___        _ _      _    
 # |_   _| |_  ___ _ __  ___/ __|_ __ _(_) |_ __| |_  
 #   | | | ' \/ -_) '  \/ -_)__ \ V  V / |  _/ _| ' \ 
@@ -6,7 +6,7 @@
 #                                                    
 
 # Default folder
-themes_path="$HOME/dotfiles/waybar/themes"
+themes_path="$HOME/.config/waybar/themes"
 
 # Init database
 listThemes=""
@@ -19,7 +19,7 @@ for value in $options
 do
     if [ ! $value == "$themes_path" ]; then
         if [ $(find $value -maxdepth 1 -type d | wc -l) = 1 ]; then
-            result=$(echo $value | sed "s#$HOME/dotfiles/waybar/themes/#/#g")
+            result=$(echo $value | sed "s#$HOME/.config/waybar/themes/#/#g")
             IFS='/' read -ra arrThemes <<< "$result"
             listThemes[${#listThemes[@]}]="/${arrThemes[1]};$result"
             if [ -f $themes_path$result/config.sh ]; then
@@ -34,11 +34,12 @@ done
 
 # Show rofi dialog
 listNames=${listNames::-2}
-choice=$(echo -e "$listNames" | rofi -dmenu -replace -config ~/dotfiles/rofi/config-theme.rasi -no-show-icons -width 30 -p "Themes" -format i) 
+#choice=$(echo -e "$listNames" | rofi -dmenu -replace -config ~/.config/rofi/config-theme.rasi -no-show-icons -width 30 -p "Themes" -format i) 
+choice=$(echo -e "$listNames" | fuzzel -d --index -format i)
 
 # Set new theme by writing the theme information to ~/.cache/.themestyle.sh
 if [ "$choice" ]; then
     echo "Loading waybar theme..."
     echo "${listThemes[$choice+1]}" > ~/.cache/.themestyle.sh
-    ~/dotfiles/waybar/launch.sh
+    ~/.config/waybar/launch.sh
 fi
